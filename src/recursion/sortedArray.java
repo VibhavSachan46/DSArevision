4be8aecc7d38db02440b8ffcf078905ebf0d60a3
Vibhav <vibhavsachan743@gmail.com>
2023-04-03 22:42:25 +0530

    issorted recursion

---
+++ src/recursion/sortedArray.java
@@ -0,0 +1,19 @@
+package recursion;
+
+public class sortedArray
+{
+    public static void main(String[] args) {
+        int[] arr= {1,2,3,4,5,6,4};
+        System.out.println(isSorted(arr,0));
+    }
+
+    static boolean isSorted(int[] arr,int i)
+    {
+        if(i == arr.length-1)
+        {
+            return true;
+        }
+
+        return arr[i]<arr[i+1] && isSorted(arr,i+1);
+    }
+}
