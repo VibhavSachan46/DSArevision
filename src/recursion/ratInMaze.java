03106f3e784d6d7810856d62a127c63883cff4e7
Vibhav <vibhavsachan743@gmail.com>
2023-04-07 23:18:48 +0530

    path with obstacle

--- src/recursion/ratInMaze.java
+++ src/recursion/ratInMaze.java
@@ -7,7 +7,60 @@ public class ratInMaze
     public static void main(String[] args) {
 //        System.out.println(count(3,3));
 //        path("",3,3);
-        System.out.println(pathRet("",3,3));
+        boolean[][] maze = {
+                {true,true,true},
+                {true,false,true},
+                {true,true,true}
+        };
+        pathwithObstacles("",maze,0,0);
+    }
+
+    static void pathwithObstacles(String p,boolean[][] maze, int r,int c)
+    {
+        if(r==maze.length-1 && c== maze[0].length-1)
+        {
+            System.out.println(p);
+            return;
+        }
+        if(!maze[r][c])
+        {
+            return;
+        }
+        if(r<maze.length-1)
+        {
+            pathwithObstacles(p+'D',maze,r+1,c);
+        }
+
+        if(c<maze[0].length-1)
+        {
+            pathwithObstacles(p+'R',maze,r,c+1);
+        }
+    }
+
+    static ArrayList<String> pathRetDiagonal(String p, int r, int c)
+    {
+        if(r==1 && c==1)
+        {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> list = new ArrayList<>();
+        if(r>1)
+        {
+            list.addAll(pathRetDiagonal(p+'V',r-1,c));
+        }
+
+        if(c>1)
+        {
+            list.addAll(pathRetDiagonal(p+'H',r,c-1));
+        }
+
+        if(r>1 && c>1)
+        {
+            list.addAll(pathRetDiagonal(p+'D',r-1,c-1));
+        }
+        return list;
     }
 
 
